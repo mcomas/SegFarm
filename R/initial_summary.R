6d3ab1347377af0ed0e59c37c90e0efc3d753e4b
marc_comas <marc.comas@gmail.com>
2015-01-30 14:58:45 +0100

    S'han fet les correccions per a la importació de dades dels Excels.

--- R/initial_summary.R
+++ R/initial_summary.R
@@ -3,14 +3,21 @@ library(lubridate)
 library(dplyr)
 library(reshape2)
 
-guard_mes = read.xls("OF_Guardia_Febrer.xls", encoding= 'latin1', stringsAsFactors = F)
-suppressWarnings(rel_ofabs <- read.xls("Relacio_OF_ABS.xls", pattern = "N.Of.", encoding= 'latin1', stringsAsFactors = F))
-res = read.xls("Resultats_Enquesta_guardies_novembre_11desembre-2.xls", encoding= 'latin1', stringsAsFactors = F)
-guard_mes$Numero.Oficina = as.numeric(gsub(',', '', guard_mes$Numero.Oficina))
+guard_mes = read.xls("data/OF_Guardia_novembre-desembre.xls", encoding= 'latin1', stringsAsFactors = F)
+suppressWarnings(rel_ofabs <- read.xls("data/Relacio_OF_ABS.xls", pattern = "N.Of.", encoding= 'latin1', stringsAsFactors = F))
+res = read.xls("data/Resultats_Enquesta_guardies_novembre_11desembre-2.xls", encoding= 'latin1', stringsAsFactors = F)
 
+### Correccions inicials de les taules importades
+guard_mes$OF.núm. = as.numeric(gsub(',', '', guard_mes$OF.núm.))
+res$OFnum[res$OFnum == 943] = 94
+rel_ofabs = rbind(rel_ofabs,
+                  c(NA,126,NA, NA,NA,"Urbanes",  NA,  NA,  NA),
+                  c(NA,351,NA, NA,NA,"Semiurbana",  NA,  NA,  NA))
 
 res$data = as.Date(res$data, "%Y-%m-%d")
 res$tipus = with(rel_ofabs, Tipus.ABS[match(res$OFnum, N.Of.)])
+
+
 
 summ = function(.data) summarize(.data,
   'producte' = length(idenquesta), 
