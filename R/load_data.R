d2306bcb690746b9ffd214e7df4176bc900d84ec
mcomas <marc@Marcs-Mac-mini.local>
2015-02-23 21:12:35 +0100

    Es crea el segon informe i es fan les modificacions corresponents

--- R/load_data.R
+++ R/load_data.R
@@ -7,18 +7,20 @@ library(stringr)
 ROOT = ''
 if(exists('Rmd_script')) ROOT = '../'
 
-guard_mes = read.xls(paste0(ROOT, "data/OF_Guardia_novembre-desembre.xls"), encoding= 'latin1', stringsAsFactors = F)
+#Guardies del mes
+guard_mes = read.xls(paste0(ROOT, "data/Relacio_OF_participants-actives.xls"), encoding= 'latin1', stringsAsFactors = F)
 suppressWarnings(rel_ofabs <- read.xls(paste0(ROOT, "data/Relacio_OF_ABS.xls"), pattern = "N.Of.", encoding= 'latin1', stringsAsFactors = F))
-res = read.xls(paste0(ROOT, "data/Resultats_Enquesta_guardies_novembre_11desembre-2.xls"), encoding= 'latin1', stringsAsFactors = F)
-nov = read.xls(paste0(ROOT, 'data/LLista_Novembre_2014_Nova.xls'), encoding = 'latin1', stringsAsFactors = F)
+#Dispensacions del mes
+res = read.xls(paste0(ROOT, "data/Dades_Enquesta_febrer_01_19.xls"), encoding= 'latin1', stringsAsFactors = F)
+#nov = read.xls(paste0(ROOT, 'data/LLista_Novembre_2014_Nova.xls'), encoding = 'latin1', stringsAsFactors = F)
 
 ### Correccions inicials de les taules importades
-res$preu = ifelse(res$preu == "", '0', res$preu)
-res$preu =  gsub(';', '.', res$preu)
-res$preu =  gsub(':', '.', res$preu)
-res$preu = as.numeric(res$preu)
-guard_mes$OF.núm. = as.numeric(gsub(',', '', guard_mes$OF.núm.))
-res$OFnum[res$OFnum == 943] = 94
+#res$preu = ifelse(res$preu == "", '0', res$preu)
+#res$preu =  gsub(';', '.', res$preu)
+#res$preu =  gsub(':', '.', res$preu)
+#res$preu = as.numeric(res$preu)
+guard_mes$Num.Of = as.numeric(gsub(',', '', guard_mes$Num.Of))
+
 rel_ofabs = rbind(rel_ofabs,
                   c(NA,126,NA, NA,NA,"Urbanes",  NA,  NA,  NA),
                   c(NA,351,NA, NA,NA,"Semiurbana",  NA,  NA,  NA))
@@ -30,3 +32,4 @@ res$horari = ifelse( 22 <= hora | hora <= 8, 'nocturn (22h-9h)', 'diurn (9h-22h)
 res$h = factor(str_sub(res$hora, 1, 2), levels=sprintf("%02d", c(9:23, 0:8)))
 
 res$tipus = with(rel_ofabs, Tipus.ABS[match(res$OFnum, N.Of.)])
+
