79bfe41a07f3d7d02d963f7a1385ad1f49ce72e1
mcomas <marc@Marcs-Mac-mini.local>
2015-04-01 19:35:01 +0200

    S'incorpora l'informe del febrer

--- R/load_data.R
+++ R/load_data.R
@@ -8,28 +8,21 @@ ROOT = ''
 if(exists('Rmd_script')) ROOT = '../'
 
 #Guardies del mes
-guard_mes = read.xls(paste0(ROOT, "data/Relacio_OF_participants-actives.xls"), encoding= 'latin1', stringsAsFactors = F)
-suppressWarnings(rel_ofabs <- read.xls(paste0(ROOT, "data/Relacio_OF_ABS.xls"), pattern = "N.Of.", encoding= 'latin1', stringsAsFactors = F))
-#Dispensacions del mes
-res = read.xls(paste0(ROOT, "data/Dades_Enquesta_febrer_01_19.xls"), encoding= 'latin1', stringsAsFactors = F)
-#nov = read.xls(paste0(ROOT, 'data/LLista_Novembre_2014_Nova.xls'), encoding = 'latin1', stringsAsFactors = F)
+guard_mes = read.xls(paste0(ROOT, "data/2015-02/OF_Participants_Enq_Guardies_Febrer.xls"), encoding='latin1', stringsAsFactors = F)
+guard_mes = guard_mes %>% mutate(
+  Dia = as.Date(Dia, "%Y-%m-%d"))
 
-### Correccions inicials de les taules importades
-#res$preu = ifelse(res$preu == "", '0', res$preu)
-#res$preu =  gsub(';', '.', res$preu)
-#res$preu =  gsub(':', '.', res$preu)
-#res$preu = as.numeric(res$preu)
-guard_mes$Num.Of = as.numeric(gsub(',', '', guard_mes$Num.Of))
 
+rel_ofabs <- read.xls(paste0(ROOT, "data/Relacio_OF_ABS.xls"), pattern = "N.Of.", encoding= 'latin1', stringsAsFactors = F)
 rel_ofabs = rbind(rel_ofabs,
                   c(NA,126,NA, NA,NA,"Urbanes",  NA,  NA,  NA),
                   c(NA,351,NA, NA,NA,"Semiurbana",  NA,  NA,  NA))
-
-guard_mes$Dia = as.Date(guard_mes$Dia, "%Y-%m-%d")
-res$data = as.Date(res$data, "%Y-%m-%d")
-hora = as.numeric(str_sub(res$hora, 1, 2))
-res$horari = ifelse( 22 <= hora | hora <= 8, 'nocturn (22h-9h)', 'diurn (9h-22h)')
-res$h = factor(str_sub(res$hora, 1, 2), levels=sprintf("%02d", c(9:23, 0:8)))
-
-res$tipus = with(rel_ofabs, Tipus.ABS[match(res$OFnum, N.Of.)])
+#Dispensacions del mes
+res = read.xls(paste0(ROOT, "data/2015-02/Enquesta_guardies_febrer.xls"), encoding= 'latin1', stringsAsFactors = F)
+res = res %>% mutate(
+  data = as.Date(data, "%Y-%m-%d"),
+  hora = as.numeric(str_sub(hora, 1, 2)),
+  horari = ifelse( 22 <= hora | hora <= 8, 'nocturn (22h-9h)', 'diurn (9h-22h)'),
+  h = factor(str_sub(res$hora, 1, 2), levels=sprintf("%02d", c(9:23, 0:8))),
+  tipus = rel_ofabs$Tipus.ABS[match(OFnum, rel_ofabs$N.Of.)])
 
